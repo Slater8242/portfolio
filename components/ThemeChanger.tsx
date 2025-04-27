9e4a246dcd115d01c90a0581a1171834efd1f9b9
Riad <riad-esrefov@mail.ru>
2025-04-28 02:49:11 +0300

    theme changer fix

--- components/ThemeChanger.tsx
+++ components/ThemeChanger.tsx
@@ -4,7 +4,7 @@ import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
 
 export default function ThemeToggle() {
-  const { theme, setTheme } = useTheme()
+  const { setTheme, resolvedTheme } = useTheme()
   const [mounted, setMounted] = useState(false)
 
   useEffect(() => setMounted(true), [])
@@ -13,10 +13,10 @@ export default function ThemeToggle() {
 
   return (
     <button
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
       className="absolute top-4 right-4 p-2 cursor-pointer rounded-full border border-gray-300 dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-800"
     >
-      {theme === 'dark' ? '🌞' : '🌙'}
+      {resolvedTheme === 'dark' ? '🌞' : '🌙'}
     </button>
   )
 }
